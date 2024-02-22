41ba0b7cb228a860b2c70fa370f45a2dfc45cd5a
Alexander Hernandez Castro <79608658+ArukuSynthesis@users.noreply.github.com>
2024-02-22 04:31:04 +0000

    Cambio a ejemplo base

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,40 @@
+import altair as alt
+import numpy as np
+import pandas as pd
 import streamlit as st
-from pymongo import MongoClient
 
-st.title('Prueba de conexión a MongoDB')
+"""
+# Welcome to Streamlit!
 
-def connection():
-    return MongoClient('mongodb+srv://',DB_USERNAME,':',DB_PASSWORD,',@prediccion2024.q7xsfjw.mongodb.net/')
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
-conexion = connection()
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
-def getData():
-    db = conexion.get_database('sample_restaurants')
-    collection = db.get_collection('restaurants')
-    items = collection.find()
-    return list(items)
+num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-datos = getData()
-st.write(datos)
+indices = np.linspace(0, 1, num_points)
+theta = 2 * np.pi * num_turns * indices
+radius = indices
+
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
+
+df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),
+})
+
+st.altair_chart(alt.Chart(df, height=700, width=700)
+    .mark_point(filled=True)
+    .encode(
+        x=alt.X("x", axis=None),
+        y=alt.Y("y", axis=None),
+        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+    ))
